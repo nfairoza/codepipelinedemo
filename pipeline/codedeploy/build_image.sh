36e27ad2e64d9085ccc311c880d7d76f5a127068
Noor <nfairoza@gmail.com>
2020-06-02 03:37:04 +0000

    pipeline added

---
+++ pipeline/codedeploy/build_image.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Building docker IMAGE "
+#cd  ../dockerimage
+echo "Switching directory " `pwd` 
+echo "ls" `ls`
+
+pgrep -f docker > /dev/null || echo "starting docker"
+export IMAGE_NAME=239144702963.dkr.ecr.us-west-2.amazonaws.com/cicddemo
+TAG=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
+export TAG=$TAG
+docker ps
+docker images
+docker build -f pipeline/dockerimage/Dockerfile -t $IMAGE_NAME:latest .
+docker tag $IMAGE_NAME:latest $IMAGE_NAME:$TAG
---
+++ pipeline/codedeploy/preq.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt-get update
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    unzip \
+    apt-utils \
+    python-minimal \
+    systemd
+echo  ********************************* Installed apt preqs  *********************************
+curl -fsSL https://get.docker.com -o get-docker.sh
+bash get-docker.sh
+docker --version
+echo  ********************************* Installed Docker  *********************************
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+/usr/local/bin/pip install awscli --upgrade 
+cp /usr/local/bin/aws /usr/bin/
+aws --version
+echo  ********************************* Starting docker  *********************************
+systemctl start docker
+docker ps
+service docker start
+docker images
+echo  ********************************* End of preq.sh *********************************
