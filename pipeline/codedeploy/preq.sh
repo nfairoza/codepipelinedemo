1e7aa715478e45d7fa0f3e81cc659d43dd56ca5d
Noor <nfairoza@gmail.com>
2020-06-09 03:07:59 +0000

    preq and buildspec changed

--- pipeline/codedeploy/preq.sh
+++ pipeline/codedeploy/preq.sh
@@ -24,4 +24,13 @@ systemctl start docker
 docker ps
 service docker start
 docker images
+echo  ********************************* Setup Env vairiables *********************************
+ACCOUNT_ID="$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .accountId)"
+REGION=$(curl -sq http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+REGION=${REGION: :-1}
+export ACCOUNT_ID=$ACCOUNT_ID
+export REGION=$REGION
+echo "REGION=$REGION" >> /etc/bashrc
+echo "ACCOUNT_ID=$ACCOUNT_ID" >> /etc/bashrc
+source /etc/bashrc
 echo  ********************************* End of preq.sh *********************************
