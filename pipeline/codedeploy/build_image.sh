ce95c4057235563180a754d442c9187c69de9219
Noor <nfairoza@gmail.com>
2020-06-11 19:23:30 +0000

    updated index

--- pipeline/codedeploy/build_image.sh
+++ pipeline/codedeploy/build_image.sh
@@ -5,11 +5,9 @@ echo "Switching directory " `pwd`
 echo "ls" `ls`
 
 pgrep -f docker > /dev/null || echo "starting docker"
-export IMAGE_NAME=239144702963.dkr.ecr.us-west-2.amazonaws.com/cicddemo
+export IMAGE_NAME=$1
 TAG=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
 export TAG=$TAG
-docker ps
-docker images
 docker pull $IMAGE_NAME
 docker build -f pipeline/dockerimage/Dockerfile -t $IMAGE_NAME:latest .
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$TAG
