84713e71ee76ba03b6049461cb53879311acfde7
Noor <nfairoza@gmail.com>
2020-06-12 19:45:28 +0000

    updated index

--- pipeline/codedeploy/build_image.sh
+++ pipeline/codedeploy/build_image.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo "Building docker IMAGE "
-#cd  ../dockerimage
 echo "Switching directory " `pwd` 
 echo "ls" `ls`
-
 pgrep -f docker > /dev/null || echo "starting docker"
 export IMAGE_NAME=$1
 TAG=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
